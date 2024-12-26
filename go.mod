db4254ec55e8565b79e7313036d900c989781f17
asepopov <asepopov@avito.ru>
2024-12-26 16:32:21 +0300

    add tests

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/front-go/auth
 go 1.22.8
 
 require (
+	github.com/golang/mock v1.6.0
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/lib/pq v1.10.9
 	google.golang.org/grpc v1.68.1
@@ -10,8 +11,12 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
 	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/service/service_test.go
@@ -0,0 +1,78 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"github.com/front-go/auth/internal/repository"
+	"github.com/front-go/auth/pkg/auth"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestService_Signup(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockDbRepo := NewMockDbRepo(ctrl)
+
+	t.Run("ok", func(t *testing.T) {
+		ctx := context.Background()
+		in := &auth.SignupIn{
+			Username:        "test",
+			Password:        "123",
+			ConfirmPassword: "123",
+		}
+
+		mockDbRepo.EXPECT().Insert(gomock.Any(), in.Username, in.Password).Return(nil)
+
+		srv := NewService(mockDbRepo)
+		_, err := srv.Signup(ctx, in)
+		assert.NoError(t, err)
+	})
+
+	t.Run("password_not_match", func(t *testing.T) {
+		ctx := context.Background()
+		in := &auth.SignupIn{
+			Password:        "123",
+			ConfirmPassword: "321",
+		}
+
+		srv := NewService(mockDbRepo)
+		_, err := srv.Signup(ctx, in)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "password mismatch")
+	})
+
+	t.Run("fail call insert", func(t *testing.T) {
+		ctx := context.Background()
+		mockError := errors.New("mock error")
+		in := &auth.SignupIn{
+			Username:        "test",
+			Password:        "123",
+			ConfirmPassword: "123",
+		}
+
+		mockDbRepo.EXPECT().Insert(gomock.Any(), in.Username, in.Password).Return(mockError)
+
+		srv := NewService(mockDbRepo)
+		_, err := srv.Signup(ctx, in)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "failed to insert user")
+	})
+
+	t.Run("fail_call_insert_err_already_exist", func(t *testing.T) {
+		ctx := context.Background()
+		mockError := repository.ErrAlreadyExist
+		in := &auth.SignupIn{
+			Username:        "test",
+			Password:        "123",
+			ConfirmPassword: "123",
+		}
+
+		mockDbRepo.EXPECT().Insert(gomock.Any(), in.Username, in.Password).Return(mockError)
+
+		srv := NewService(mockDbRepo)
+		_, err := srv.Signup(ctx, in)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "Пользователь уже существует")
+	})
+}
