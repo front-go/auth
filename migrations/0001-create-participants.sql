8400db374f02c61ae6df6f3bcf8ae9296d3c1383
asepopov <asepopov@avito.ru>
2025-02-20 18:04:05 +0300

    add config and linting

---
+++ migrations/0001-create-participants.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS participant (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS participant;
